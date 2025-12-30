2363ae2996722e2b4ea8cda701952c2ba3073c19
justmatedev <justmatedev@gmail.com>
2025-12-29 21:02:34 -0300

    fix: menu names

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,29 @@
-import type { Metadata } from "next";
-import { Google_Sans_Code } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from "next"
+import { Google_Sans_Code } from "next/font/google"
+import "./globals.css"
 
 const googleSansCode = Google_Sans_Code({
   variable: "--font-google-sans-code",
   subsets: ["latin"],
   adjustFontFallback: false,
-});
+})
 
 export const metadata: Metadata = {
   title: "JustMate - Desenvolvedor JavaScript",
-  description: "Portfólio de Mateus Justino (JustMate), desenvolvedor especializado em JavaScript, React Native e ReactJS. Transformando ideias em experiências digitais.",
-};
+  description:
+    "Portfólio de Mateus Justino (JustMate), desenvolvedor especializado em JavaScript, React Native e ReactJS. Transformando ideias em experiências digitais.",
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang="pt-BR">
-      <body
-        className={`${googleSansCode.variable} antialiased`}
-      >
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body className={`${googleSansCode.variable} antialiased`}>
         {children}
       </body>
     </html>
-  );
+  )
 }
